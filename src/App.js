3695569a6183168f079faf124279f4c0ca1fad57
taqnar <muzammilthottathil.official@gmail.com>
2021-08-09 11:46:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Breaking Bad Characters</h1>
+        <p>{`App to list Breaking Bad Characters & their Quotes`}</p>
       </header>
     </div>
   );
